06e36f61aca5c5ff7e9934921d2972282fcc5f19
Super Dev <superdev1031@gmail.com>
2024-12-14 11:41:13 -0500

    create home page

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div
+      className="ui raised very padded text container segment"
+      style={{ marginTop: "80px" }}
+    >
+      <h3 className="ui header">Home</h3>
+      <p>Lorem</p>
+    </div>
+  );
+}
+
+export default Home;
