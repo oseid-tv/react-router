0000f027ba1fc83784a6dba0876299f4bd920111
Super Dev <superdev1031@gmail.com>
2024-12-15 12:45:36 -0500

    create react-potal

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Modal.css";
 
 function About() {
   return (
-    <div
-      className="ui raised very padded text container segment"
-      style={{ marginTop: "80px" }}
-    >
-      <h3 className="ui header">About</h3>
-      <p>Lorem</p>
+    <div>
+      <div
+        className="ui raised very padded text container segment"
+        style={{ marginTop: "80px" }}
+      >
+        <h3 className="ui header">About</h3>
+        <p>Lorem</p>
+      </div>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Modal from "./Modal";
 
 function Contact(props) {
   return (
     <div>
+      <Modal />
       <div
         className="ui raised very padded text container segment"
         style={{ marginTop: "80px" }}
